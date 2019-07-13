21970a37fbde633e754bbf85234e5d2f92890c53
HiromuAndo <tqgqxxjuzaa8hbcwjv7n@docomo.ne.jp>
2019-07-13 12:23:09 +0900

    [Update]device周りとヘッダー・フッター

--- app/models/enduser.rb
+++ app/models/enduser.rb
@@ -1,11 +1,20 @@
 class Enduser < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  has_many :orders, dependent: :destroy
+  has_many :shippings, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+  has_many :carts, dependent: :destroy
 
-has_many :orders, dependent: :destroy
-has_many :shippings, dependent: :destroy
-has_many :contacts, dependent: :destroy
-has_many :carts, dependent: :destroy
-
-
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :tel, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_044251) do
+ActiveRecord::Schema.define(version: 2019_07_10_150122) do
 
   create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
     t.string "last_name_kana"
-    t.text "email"
-    t.text "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "artists", force: :cascade do |t|
@@ -49,18 +59,28 @@ ActiveRecord::Schema.define(version: 2019_07_10_044251) do
   end
 
   create_table "endusers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.string "first_name"
     t.string "last_name"
     t.string "first_name_kana"
     t.string "last_name_kana"
-    t.text "email"
     t.string "postcode"
     t.text "address"
     t.string "tel"
-    t.text "password"
-    t.boolean "is_deleted"
+    t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_endusers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_endusers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
@@ -120,32 +140,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_044251) do
     t.integer "track"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.text "email", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "last_name_kana", null: false
-    t.string "postcode", null: false
-    t.text "address", null: false
-    t.string "tel", null: false
-    t.text "password", null: false
-    t.boolean "is_deleted", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
